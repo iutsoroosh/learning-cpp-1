b48d44e6f56e649d780e21759228964b36111af0
Soroosh Hassanpour <iutsoroosh@users.noreply.github.com>
2024-08-23 05:43:04 +0000

    ch01_02

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,10 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Hello, " << *msg << std::endl;
+}
